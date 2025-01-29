96070e53aa8454b7e8ed539ada08bec1e9a16a2b
boorownie <probitanima11@gmail.com>
2025-01-29 13:01:32 +0900

    practice2 - refactoring properties

--- src/test/java/nextstep/app/oauth2/LoginRedirectFilterTest.java
+++ src/test/java/nextstep/app/oauth2/LoginRedirectFilterTest.java
@@ -4,28 +4,32 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
-class GithubLoginRedirectFilterTest {
+class LoginRedirectFilterTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void redirectTest() throws Exception {
+    void github() throws Exception {
         String requestUri = "/oauth2/authorization/github";
-        String expectedRedirectUri = "https://github.com/login/oauth/authorize" +
-                "?client_id=Ov23liTBhugSIcf8VX1v" +
-                "&response_type=code" +
-                "&scope=read:user" +
-                "&redirect_uri=http://localhost:8080/login/oauth2/code/github";
 
         mockMvc.perform(MockMvcRequestBuilders.get(requestUri))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.redirectedUrl(expectedRedirectUri));
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+    }
+
+    @Test
+    void google() throws Exception {
+        String requestUri = "/oauth2/authorization/google";
+
+        mockMvc.perform(MockMvcRequestBuilders.get(requestUri))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
     }
 }
