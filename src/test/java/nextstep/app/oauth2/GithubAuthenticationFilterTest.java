6393c292c5e83f3875bf589e210c562e6fd6d3c1
boorownie <probitanima11@gmail.com>
2025-01-30 19:42:54 +0900

    practice2 - refactoring test

--- src/test/java/nextstep/app/oauth2/GithubAuthenticationFilterTest.java
+++ src/test/java/nextstep/app/oauth2/GithubAuthenticationFilterTest.java
@@ -1,12 +1,11 @@
 package nextstep.app.oauth2;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.HttpSession;
-import nextstep.security.context.HttpSessionSecurityContextRepository;
-import nextstep.security.context.SecurityContext;
+import nextstep.app.domain.Member;
+import nextstep.app.domain.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,9 +18,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -32,52 +33,80 @@ class GithubAuthenticationFilterTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
     @BeforeEach
     void setupMockServer() throws Exception {
-        stubForAccessToken();
-        stubForUser();
+        UserStub userB = new UserStub("b", "b_access_token", "b@b.com", "b", "b_avatar_url");
+        setupUserStub(userB);
+
+        UserStub userC = new UserStub("c", "c_access_token", "c@c.com", "c", "c_avatar_url");
+        setupUserStub(userC);
     }
 
-    @Test
-    void redirectAndRequestGithubAccessToken() throws Exception {
-        String requestUri = "/login/oauth2/code/github?code=mock_code";
+    @ParameterizedTest
+    @MethodSource("userProvider")
+    void authenticationFilter(UserStub user) throws Exception {
+        String requestUri = "/login/oauth2/code/github?code=" + user.code;
 
         mockMvc.perform(MockMvcRequestBuilders.get(requestUri))
+                .andDo(print())
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.redirectedUrl("/"))
-                .andExpect(request -> {
-                    HttpSession session = request.getRequest().getSession();
-                    assert session != null;
-                    SecurityContext context = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
-                    assertThat(context).isNotNull();
-                    assertThat(context.getAuthentication()).isNotNull();
-                    assertThat(context.getAuthentication().isAuthenticated()).isTrue();
-                    assertThat(context.getAuthentication().getPrincipal()).isEqualTo("a@a.com");
-                });
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/"));
+
+        Member savedMember = memberRepository.findByEmail(user.email).get();
+        assertThat(savedMember).isNotNull();
+        assertThat(savedMember.getEmail()).isEqualTo(user.email);
+        assertThat(savedMember.getName()).isEqualTo(user.name);
     }
 
-    private static void stubForAccessToken() throws JsonProcessingException {
-        Map<String, String> responseBody = new HashMap<>();
-        responseBody.put("access_token", "mock_access_token");
-        responseBody.put("token_type", "bearer");
-        String jsonResponse = new ObjectMapper().writeValueAsString(responseBody);
+    private void setupUserStub(UserStub user) throws Exception {
+        Map<String, String> accessTokenResponseBody = new HashMap<>();
+        accessTokenResponseBody.put("access_token", user.accessToken);
+        accessTokenResponseBody.put("token_type", "bearer");
+        String accessTokenJsonResponse = new ObjectMapper().writeValueAsString(accessTokenResponseBody);
 
         stubFor(post(urlEqualTo("/login/oauth/access_token"))
+                .withRequestBody(containing("code=" + user.code))
                 .willReturn(aResponse()
                         .withHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-                        .withBody(jsonResponse)));
-    }
+                        .withBody(accessTokenJsonResponse)));
 
-    private static void stubForUser() throws JsonProcessingException {
         Map<String, String> userProfile = new HashMap<>();
-        userProfile.put("email", "a@a.com");
-        userProfile.put("name", "a");
-        userProfile.put("avatar_url", "");
+        userProfile.put("email", user.email);
+        userProfile.put("name", user.name);
+        userProfile.put("avatar_url", user.avatarUrl);
         String profileJsonResponse = new ObjectMapper().writeValueAsString(userProfile);
 
         stubFor(get(urlEqualTo("/user"))
+                .withHeader("Authorization", equalTo("Bearer " + user.accessToken))
                 .willReturn(aResponse()
                         .withHeader(HttpHeaders.CONTENT_TYPE, "application/json")
                         .withBody(profileJsonResponse)));
+
+    }
+
+    static Stream<UserStub> userProvider() {
+        return Stream.of(
+                new UserStub("b", "b_access_token", "b@b.com", "b", "b_avatar_url"),
+                new UserStub("c", "c_access_token", "c@c.com", "c", "c_avatar_url")
+        );
+    }
+
+    static class UserStub {
+        String code;
+        String accessToken;
+        String email;
+        String name;
+        String avatarUrl;
+
+        public UserStub(String code, String accessToken, String email, String name, String avatarUrl) {
+            this.code = code;
+            this.accessToken = accessToken;
+            this.email = email;
+            this.name = name;
+            this.avatarUrl = avatarUrl;
+        }
     }
 }
