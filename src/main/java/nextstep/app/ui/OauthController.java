dd99a56c7bf900065dfd51b4cefe7f0fd0992323
pilming44 <pilming44@naver.com>
2025-02-19 00:22:01 +0900

    feat: 1-2 단순구현

--- src/main/java/nextstep/app/ui/OauthController.java
+++ src/main/java/nextstep/app/ui/OauthController.java
@@ -45,8 +45,24 @@ public class OauthController {
         return "redirect:" + url;
     }
 
+    @GetMapping("/oauth2/authorization/google")
+    public String googleAuthenticationRequest() {
+        String clientId = "Ov23liTBhugSIcf8VX1v";
+        String responseType = "code";
+        String scope = "email%20profile";
+        String redirectUrl = "http://localhost:8080/login/oauth2/code/google";
+
+        String url = "https://accounts.google.com/o/oauth2/v2/auth" +
+                "?client_id=" + clientId +
+                "&response_type=" + responseType +
+                "&scope=" + scope +
+                "&redirect_uri=" + redirectUrl;
+
+        return "redirect:" + url;
+    }
+
     @GetMapping("/login/oauth2/code/github")
-    public String redirectFromHitHubWithCode(
+    public String redirectFromGitHubWithCode(
             HttpServletRequest request,
             HttpServletResponse response,
             @RequestParam("code") String code) {
@@ -94,6 +110,70 @@ public class OauthController {
         //사용자 조회 혹은 저장
         Member member = memberRepository.findByEmail(email)
                 .orElse(memberRepository.save(new Member(email, null, name, avatarUrl, Set.of("USER"))));
+
+        UsernamePasswordAuthenticationToken authenticated = UsernamePasswordAuthenticationToken
+                .authenticated(member.getEmail(), null, member.getRoles());
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authenticated);
+        SecurityContextHolder.setContext(context);
+        this.securityContextRepository.saveContext(context, request, response);
+        return "redirect:/";
+    }
+
+    @GetMapping("/login/oauth2/code/google")
+    public String redirectFromGoogleWithCode(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @RequestParam("code") String code) {
+        //토큰요청
+        String accessTokenRequestUrl = "http://localhost:8089/token";
+
+        Map<String, String> accessTokenRequestBody = new HashMap<>();
+        accessTokenRequestBody.put("code", code);
+
+        HttpHeaders accessTokenHeaders = new HttpHeaders();
+        accessTokenHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Map<String, String>> accessTokenRequestEntity = new HttpEntity<>(accessTokenRequestBody, accessTokenHeaders);
+
+        ResponseEntity<Map> accessTokenResponseEntity = restTemplate.exchange(
+                accessTokenRequestUrl, HttpMethod.POST, accessTokenRequestEntity, Map.class);
+        Map<String, Object> accessTokenBody = (Map<String, Object>) accessTokenResponseEntity.getBody();
+        if (Objects.isNull(accessTokenBody)) {
+            throw new RuntimeException();
+        }
+        String accessToken = accessTokenBody.get("access_token").toString();
+        String expiresIn = accessTokenBody.get("expires_in").toString();
+        String refreshToken = accessTokenBody.get("refresh_token").toString();
+        String scope = accessTokenBody.get("scope").toString();
+        String tokenType = accessTokenBody.get("token_type").toString();
+        String idToken = accessTokenBody.get("id_token").toString();
+
+        //리소스 조회 요청
+        String resourceRequestUrl = "http://localhost:8089/v1/userinfo";
+
+        Map<String, String> resourceRequestBody = new HashMap<>();
+        resourceRequestBody.put("access_token", accessToken);
+
+        HttpHeaders resourceHeaders = new HttpHeaders();
+        resourceHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Map<String, String>> resourceRequestEntity = new HttpEntity<>(resourceRequestBody, resourceHeaders);
+
+        ResponseEntity<Map> resourceResponseEntity = restTemplate.exchange(
+                resourceRequestUrl, HttpMethod.GET, resourceRequestEntity, Map.class);
+        Map<String, Object> resourceBody = (Map<String, Object>) resourceResponseEntity.getBody();
+        if (Objects.isNull(resourceBody)) {
+            throw new RuntimeException();
+        }
+        String email = resourceBody.get("email").toString();
+        String name = resourceBody.get("name").toString();
+        String pictureUrl = resourceBody.get("picture").toString();
+
+        //사용자 조회 혹은 저장
+        Member member = memberRepository.findByEmail(email)
+                .orElse(memberRepository.save(new Member(email, null, name, pictureUrl, Set.of("USER"))));
 
         UsernamePasswordAuthenticationToken authenticated = UsernamePasswordAuthenticationToken
                 .authenticated(member.getEmail(), null, member.getRoles());
