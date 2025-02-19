7b89b5e7188b2de73358bc7097710d6406cc1daf
ParkSeryu <pjh971212@gmail.com>
2025-02-19 20:41:58 +0900

    feat: 인증 URL 리다이렉트 필터 구현

--- src/main/java/nextstep/app/SecurityConfig.java
+++ src/main/java/nextstep/app/SecurityConfig.java
@@ -9,6 +9,7 @@ import nextstep.security.access.hierarchicalroles.RoleHierarchy;
 import nextstep.security.access.hierarchicalroles.RoleHierarchyImpl;
 import nextstep.security.authentication.AuthenticationException;
 import nextstep.security.authentication.BasicAuthenticationFilter;
+import nextstep.security.authentication.GithubLoginRedirectFilter;
 import nextstep.security.authentication.UsernamePasswordAuthenticationFilter;
 import nextstep.security.authorization.*;
 import nextstep.security.config.DefaultSecurityFilterChain;
@@ -59,6 +60,7 @@ public class SecurityConfig {
                         new SecurityContextHolderFilter(),
                         new UsernamePasswordAuthenticationFilter(userDetailsService()),
                         new BasicAuthenticationFilter(userDetailsService()),
+                        new GithubLoginRedirectFilter(),
                         new AuthorizationFilter(requestAuthorizationManager())
                 )
         );
