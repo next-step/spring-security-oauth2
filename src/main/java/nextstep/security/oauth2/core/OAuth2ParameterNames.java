13c80cb341d20b399af6dad689bcd535ca14781a
parkSeryu <pjh971212@gmail.com>
2025-02-24 04:14:54 +0900

    feat: OAuth2ParameterNames 추가

--- src/main/java/nextstep/security/oauth2/core/OAuth2ParameterNames.java
+++ src/main/java/nextstep/security/oauth2/core/OAuth2ParameterNames.java
@@ -6,6 +6,9 @@ public class OAuth2ParameterNames {
     public static final String RESPONSE_TYPE = "response_type";
     public static final String SCOPE = "scope";
     public static final String CODE = "code";
+    public static final String ERROR = "error";
+    public static final String ERROR_DESCRIPTION = "error_description";
+    public static final String ERROR_URI = "error_uri";
     public static final String GRANT_TYPE = "grant_type";
     public static final String AUTHORIZATION_CODE = "authorization_code";
     public static final String ACCESS_TOKEN = "access_token";
