bb9b3aaae68335916a67a9e303a79ee82f56a942
qwe5507 <apvmffkdls@gmail.com>
2025-02-25 15:38:27 +0900

    feat: OAuth AuthorizationRequestResolver 구현

--- src/main/java/nextstep/oauth2/OAuth2AuthorizationRequestRedirectFilter.java
+++ src/main/java/nextstep/oauth2/OAuth2AuthorizationRequestRedirectFilter.java
@@ -4,47 +4,21 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import nextstep.oauth2.registration.ClientRegistration;
 import nextstep.oauth2.registration.ClientRegistrationRepository;
-import nextstep.security.access.MvcRequestMatcher;
-import nextstep.security.access.RequestMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 
 public class OAuth2AuthorizationRequestRedirectFilter extends OncePerRequestFilter {
     public static final String DEFAULT_AUTHORIZATION_REQUEST_BASE_URI = "/oauth2/authorization/";
-    private final RequestMatcher requiresAuthenticationRequestMatcher;
     private final OAuth2AuthorizationRequestResolver authorizationRequestResolver;
 
     public OAuth2AuthorizationRequestRedirectFilter(final ClientRegistrationRepository clientRegistrationRepository) {
-        this.requiresAuthenticationRequestMatcher = new MvcRequestMatcher(DEFAULT_AUTHORIZATION_REQUEST_BASE_URI);
-        this.authorizationRequestResolver = new OAuth2AuthorizationRequestResolver(clientRegistrationRepository);
+        this.authorizationRequestResolver = new OAuth2AuthorizationRequestResolver(clientRegistrationRepository, DEFAULT_AUTHORIZATION_REQUEST_BASE_URI);
     }
 
     @Override
     protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain filterChain) throws ServletException, IOException {
-//        if (!requiresAuthenticationRequestMatcher.matches(request)) {
-//            doFilter(request, response, filterChain);
-//            return;
-//        }
-//
-//        final String registrationId = extractRegistrationId(request.getRequestURI(), DEFAULT_AUTHORIZATION_REQUEST_BASE_URI);
-//        if (registrationId == null) {
-//            doFilter(request, response, filterChain);
-//            return;
-//        }
-//
-//        final ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId(registrationId);
-//        if (clientRegistration == null) {
-//            throw new OAuth2RegistrationNotFoundException(registrationId);
-//        }
-//
-        final String uriString = buildRedirectURI(clientRegistration);
-        response.sendRedirect(uriString);
-
-        // ================================
         OAuth2AuthorizationRequest authorizationRequest = this.authorizationRequestResolver.resolve(request);
         if (authorizationRequest != null) {
             sendRedirectForAuthorization(request, response, authorizationRequest);
@@ -54,20 +28,7 @@ public class OAuth2AuthorizationRequestRedirectFilter extends OncePerRequestFilt
         filterChain.doFilter(request, response);
     }
 
-    public String extractRegistrationId(String requestUri, String baseUri) {
-        if (requestUri.length() <= baseUri.length()) {
-            throw new IllegalArgumentException("Invalid request URI: " + requestUri);
-        }
-        return requestUri.substring(baseUri.length());
-    }
-
-    private String buildRedirectURI(final ClientRegistration registration) {
-        final UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(registration.getAuthorizationUri());
-        uriBuilder.queryParam("response_type", "code");
-        uriBuilder.queryParam("client_id", registration.getClientId());
-        uriBuilder.queryParam("scope", String.join(" ", registration.getScopes()));
-        uriBuilder.queryParam("redirect_uri", registration.getRedirectUri());
-
-        return uriBuilder.toUriString();
+    private void sendRedirectForAuthorization(final HttpServletRequest request, final HttpServletResponse response, final OAuth2AuthorizationRequest authorizationRequest) {
+        throw new UnsupportedOperationException("Unsupported sendRedirectForAuthorization");
     }
 }
--- src/main/java/nextstep/oauth2/OAuth2AuthorizationRequestResolver.java
+++ src/main/java/nextstep/oauth2/OAuth2AuthorizationRequestResolver.java
@@ -1,16 +1,80 @@
 package nextstep.oauth2;
 
 import jakarta.servlet.http.HttpServletRequest;
+import nextstep.oauth2.exception.OAuth2RegistrationNotFoundException;
+import nextstep.oauth2.keygen.StateGenerator;
+import nextstep.oauth2.registration.ClientRegistration;
 import nextstep.oauth2.registration.ClientRegistrationRepository;
+import nextstep.security.access.MvcRequestMatcher;
+import nextstep.security.access.RequestMatcher;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class OAuth2AuthorizationRequestResolver {
+    private static final StateGenerator DEFAULT_STATE_GENERATOR = new StateGenerator();
     private final ClientRegistrationRepository clientRegistrationRepository;
+    private final RequestMatcher requiresAuthenticationRequestMatcher;
+    private final String authorizationRequestBaseUri;
 
-    public OAuth2AuthorizationRequestResolver(final ClientRegistrationRepository clientRegistrationRepository) {
+    public OAuth2AuthorizationRequestResolver(final ClientRegistrationRepository clientRegistrationRepository,
+                                              final String authorizationRequestBaseUri) {
         this.clientRegistrationRepository = clientRegistrationRepository;
+        this.requiresAuthenticationRequestMatcher = new MvcRequestMatcher(authorizationRequestBaseUri);
+        this.authorizationRequestBaseUri = authorizationRequestBaseUri;
     }
 
     public OAuth2AuthorizationRequest resolve(HttpServletRequest request) {
+        String registrationId = resolveRegistrationId(request);
+        if (registrationId == null) {
+            return null;
+        }
 
+        return resolve(registrationId);
     }
+
+    private String resolveRegistrationId(final HttpServletRequest request) {
+        if (requiresAuthenticationRequestMatcher.matches(request)) {
+            return extractRegistrationId(request.getRequestURI(), authorizationRequestBaseUri);
+        }
+
+        return null;
+    }
+
+    public String extractRegistrationId(String requestUri, String baseUri) {
+        if (requestUri.length() <= baseUri.length()) {
+            throw new IllegalArgumentException("Invalid request URI: " + requestUri);
+        }
+        return requestUri.substring(baseUri.length());
+    }
+
+    private OAuth2AuthorizationRequest resolve(final String registrationId) {
+        if (registrationId == null) {
+            return null;
+        }
+        final ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId(registrationId);
+        if (clientRegistration == null) {
+            throw new OAuth2RegistrationNotFoundException(registrationId);
+        }
+
+        String state = DEFAULT_STATE_GENERATOR.generateState();
+        return OAuth2AuthorizationRequest.builder()
+                .authorizationUri(clientRegistration.getAuthorizationUri())
+                .clientId(clientRegistration.getClientId())
+                .redirectUri(clientRegistration.getRedirectUri())
+                .scopes(clientRegistration.getScopes())
+                .state(state)
+                .authorizationRequestUri(buildAuthorizationRequestUri(clientRegistration, state))
+                .build();
+    }
+
+    private String buildAuthorizationRequestUri(final ClientRegistration registration, String state) {
+        final UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(registration.getAuthorizationUri());
+        uriBuilder.queryParam("response_type", "code");
+        uriBuilder.queryParam("client_id", registration.getClientId());
+        uriBuilder.queryParam("scope", String.join(" ", registration.getScopes()));
+        uriBuilder.queryParam("redirect_uri", registration.getRedirectUri());
+        uriBuilder.queryParam("state", state);
+
+        return uriBuilder.toUriString();
+    }
+
 }
