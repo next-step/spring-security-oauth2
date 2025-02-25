dc6e13a442d770e78c2e3e88af946a03b2822c51
parkSeryu <pjh971212@gmail.com>
2025-02-25 23:10:11 +0900

    refactor: 주석 대신 @Nullable 애노테이션을 사용

--- src/main/java/nextstep/security/oauth2/client/authentication/GithubProfileUser.java
+++ src/main/java/nextstep/security/oauth2/client/authentication/GithubProfileUser.java
@@ -1,16 +1,18 @@
 package nextstep.security.oauth2.client.authentication;
 
+import jakarta.annotation.Nullable;
 import java.util.Map;
 
 public class GithubProfileUser implements OAuth2ProfileUser {
     private final String name;
     private final String imageUrl;
+    @Nullable
     private final String email;
 
     public GithubProfileUser(Map<String, Object> attributes) {
         this.name = (String) attributes.get("name");
         this.imageUrl = (String) attributes.get("avatar_url");
-        this.email = (String) attributes.get("email"); // 이메일이 null일 수도 있음
+        this.email = (String) attributes.get("email");
     }
 
     @Override
@@ -24,6 +26,7 @@ public class GithubProfileUser implements OAuth2ProfileUser {
     }
 
     @Override
+    @Nullable
     public String getEmail() {
         return email;
     }
